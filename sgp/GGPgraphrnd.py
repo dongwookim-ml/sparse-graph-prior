7cee4af549b6ae54896e1a4f896ab233c374c7f7
Dongwoo Kim <arongdari@gmail.com>
2016-07-22 10:15:12 +1000

    function for generating a mixture of sparse and dense network.

--- sgp/GGPgraphrnd.py
+++ sgp/GGPgraphrnd.py
@@ -24,13 +24,13 @@ def GGPgraphrnd(alpha, sigma, tau, T=0):
 
     if isinstance(alpha, collections.Iterable):
         hyper_alpha = alpha
-        alpha = np.random.gamma(hyper_alpha[0], 1./hyper_alpha[1])
+        alpha = np.random.gamma(hyper_alpha[0], 1. / hyper_alpha[1])
     if isinstance(sigma, collections.Iterable):
         hyper_sigma = sigma
-        sigma = 1. - np.random.gamma(hyper_sigma[0], 1./hyper_sigma[1])
+        sigma = 1. - np.random.gamma(hyper_sigma[0], 1. / hyper_sigma[1])
     if isinstance(tau, collections.Iterable):
         hyper_tau = tau
-        tau = np.random.gamma(hyper_tau[0], 1./hyper_tau[1])
+        tau = np.random.gamma(hyper_tau[0], 1. / hyper_tau[1])
 
     w, T = GGPrnd(alpha, sigma, tau, T)
 
@@ -53,6 +53,64 @@ def GGPgraphrnd(alpha, sigma, tau, T=0):
     D = csc_matrix((np.ones(len(new_idx[0])), (new_idx[0], new_idx[1])), shape=(g_size, g_size))  # directed multigraph
     G = D + D.T  # undirected multigraph
     nnz = G.nonzero()
-    G = csc_matrix((np.ones(len(nnz[0])), (nnz)), shape=(g_size, g_size)) # undirected simple graph
+    G = csc_matrix((np.ones(len(nnz[0])), (nnz)), shape=(g_size, g_size))  # undirected simple graph
 
     return G, D, w, w_rem, alpha, sigma, tau
+
+
+def GGPmixtureGraphrnd(s_alpha=1., s_sigma=0.5, s_tau=1., d_alpha=1., d_sigma=-1., d_tau=1., T=0):
+    if isinstance(s_alpha, collections.Iterable):
+        hyper_alpha = s_alpha
+        s_alpha = np.random.gamma(hyper_alpha[0], 1. / hyper_alpha[1])
+    if isinstance(s_sigma, collections.Iterable):
+        hyper_sigma = s_sigma
+        s_sigma = 1. - np.random.gamma(hyper_sigma[0], 1. / hyper_sigma[1])
+    if isinstance(s_tau, collections.Iterable):
+        hyper_tau = s_tau
+        s_tau = np.random.gamma(hyper_tau[0], 1. / hyper_tau[1])
+
+    if isinstance(d_alpha, collections.Iterable):
+        hyper_alpha = d_alpha
+        d_alpha = np.random.gamma(hyper_alpha[0], 1. / hyper_alpha[1])
+    if isinstance(d_sigma, collections.Iterable):
+        hyper_sigma = d_sigma
+        d_sigma = 1. - np.random.gamma(hyper_sigma[0], 1. / hyper_sigma[1])
+    if isinstance(d_tau, collections.Iterable):
+        hyper_tau = d_tau
+        d_tau = np.random.gamma(hyper_tau[0], 1. / hyper_tau[1])
+
+    s_w, s_T = GGPrnd(s_alpha, s_sigma, s_tau, T)
+    d_w, d_T = GGPrnd(d_alpha, d_sigma, d_tau, T)
+
+    if len(s_w) == 0 or len(d_w) == 0:
+        raise Exception("GGP has no atom")
+
+    cumsum_s_w = np.cumsum(s_w)
+    cumsum_d_w = np.cumsum(d_w)
+    s_w_star = cumsum_s_w[-1]
+    d_w_star = cumsum_d_w[-1]
+    d_star = np.random.poisson((s_w_star + d_w_star) ** 2)
+    if d_star == 0:
+        raise Exception("No edge in graph")
+
+    w = np.concatenate((s_w, d_w))
+    cumsum_w = np.cumsum(w)
+
+    tmp = (s_w_star + d_w_star) * np.random.random(size=(d_star, 2))
+    idx = np.digitize(tmp, cumsum_w)
+    active_nodes_idx, inv_idx = np.unique(idx.flatten(), return_inverse=True)  # active_nodes_idx[inv_idx] = idx.flatten
+    w_rem = np.sum(w) - np.sum(w[active_nodes_idx])
+    w = w[active_nodes_idx]
+    new_idx = inv_idx.reshape(idx.shape).T
+
+    sparse_nodes = [i for i, node in enumerate(active_nodes_idx) if node < len(s_w)]
+    dense_nodes = [i for i, node in enumerate(active_nodes_idx) if node >= len(s_w)]
+
+    g_size = len(active_nodes_idx)
+    D = csc_matrix((np.ones(len(new_idx[0])), (new_idx[0], new_idx[1])), shape=(g_size, g_size))  # directed multigraph
+    G = D + D.T  # undirected multigraph
+    nnz = G.nonzero()
+    G = csc_matrix((np.ones(len(nnz[0])), (nnz)), shape=(g_size, g_size))  # undirected simple graph
+    params = (s_alpha, s_sigma, s_tau, d_alpha, d_sigma, d_tau)
+
+    return G, D, w, w_rem, params, sparse_nodes, dense_nodes
--- sgp/__init__.py
+++ sgp/__init__.py
@@ -1,7 +1,7 @@
 from .GGPrnd import GGPrnd
 from .etstablernd import etstablernd
 from .GGPutils import GGPsumrnd, GGPkappa, GGPpsi
-from .GGPgraphrnd import GGPgraphrnd
+from .GGPgraphrnd import GGPgraphrnd, GGPmixtureGraphrnd
 from .BSgraphrnd import BSgraphrnd
 from .KGgraphrnd import KGgraphrnd
 from .GGPgraphmcmc import GGPgraphmcmc
